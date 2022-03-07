cf75c583079cd31e27aa1b96c1cc643b0f86873e
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-03-07 11:13:46 +0600

    Create Product model

--- home/models/product.py
+++ home/models/product.py
@@ -15,5 +15,12 @@ class Product(models.Model):
         return Product.objects.all()
     
     
+    @staticmethod
+    def get_all_products_by_catagories(catagory_id):
+        if catagory_id:
+            return Product.objects.filter(catagory =catagory_id)
+        else:
+            return Product.objects.all()
+    
     def __str__(self):
         return self.name
