9d9d5307858f68130778d1c0c5712c984aa492c5
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-03-09 11:39:08 +0600

    product details and search url create

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
-from unicodedata import name
 from django.urls import path
 from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('trending/', views.trending, name='trending'),
+    path('search/', views.search, name='search'),
+    path('product_details/<int:id>', views.details, name='product_details'),
+    # path('productBycat/', views.productBycat, name='productBycat'),
 ]
